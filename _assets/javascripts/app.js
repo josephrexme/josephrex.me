51505e3279882a2bfdb533ece3185b0e23d954a1
bl4ckdu5t <joerexko@gmail.com>
2015-03-16 23:24:42 -0500

    Built the scroll-to-fix content for home page

--- _assets/javascripts/app.js
+++ _assets/javascripts/app.js
@@ -6,24 +6,14 @@ function ready(cb) {
 }
 ready(function(){
 	// Sticky logo on home page
-	var wrap = document.querySelector('#js-wrap');
-	/*document.addEventListener('click', function(e) {
-	   if ( e.target.webkitMatchesSelector('#js-wrap') ) {
-	   }
-	}, false);*/
-	/*addEvent(wrap, 'scroll', function(){
-    	if (wrap.scrollTop > 5) {
-	    	addClass(wrap, 'fixed')
-		}else {
-			removeClass(wrap, 'fixed')
-		}
-		alert("hey");
-	});*/
-	if(wrap){
-		wrap.addEventListener("scroll", function(e) {
-		  wrap.classList.toggle('fixed', (wrap.scrollTop > 10));
+	var wrap = document.getElementById("js-wrap");
+	if(wrap && window.innerWidth >= 1024){
+		document.addEventListener("scroll", function(e) {
+			fixPoint = parseInt( getComputedStyle(wrap).getPropertyValue('top') );
+			wrap.classList.toggle( "fixed", document.body.scrollTop > fixPoint );
 		});
 	}
+	
 	// Handling Navigation
 	menuIcon = document.querySelectorAll('nav > svg');
 	Array.prototype.forEach.call( menuIcon, function(el) {
@@ -35,46 +25,3 @@ ready(function(){
 
 	});
 });
-
-var hasClass = function (el, cl) {
-        var regex = new RegExp('(?:\\s|^)' + cl + '(?:\\s|$)');
-        return !!el.className.match(regex);
-    },
-
-addClass = function (el, cl) {
-    el.className += ' ' + cl;
-},
-
-removeClass = function (el, cl) {
-    var regex = new RegExp('(?:\\s|^)' + cl + '(?:\\s|$)');
-    el.className = el.className.replace(regex, ' ');
-},
-
-toggleClass = function (el, cl) {
-    hasClass(el, cl) ? removeClass(el, cl) : addClass(el, cl);
-
-};
-var addEvent = (function () {
-	var filter = function(el, type, fn) {
-		for ( var i = 0, len = el.length; i < len; i++ ) {
-			addEvent(el[i], type, fn);
-		}
-	};
-	if ( document.addEventListener ) {
-		return function (el, type, fn) {
-			if ( el && el.nodeName || el === window ) {
-				el.addEventListener(type, fn, false);
-			} else if (el && el.length) {
-				filter(el, type, fn);
-			}
-		};
-	}
-
-	return function (el, type, fn) {
-		if ( el && el.nodeName || el === window ) {
-			el.attachEvent('on' + type, function () { return fn.call(el, window.event); });
-		} else if ( el && el.length ) {
-			filter(el, type, fn);
-		}
-	};
-})();
