807424c95deae9187b4592857b890585fc7e0bf2
Joseph Rex <joerexko@gmail.com>
2021-12-30 04:06:05 -0600

    Remove redundant font file and fix puppeteer usage

--- serverless/pdf/index.js
+++ serverless/pdf/index.js
@@ -1,29 +1,32 @@
-// const puppeteer = require('puppeteer')
+const chromium = require('chrome-aws-lambda')
 
-// const createPDF = async ({ url = 'https://resume.josephrex.me' }) => {
-//   const browser = await puppeteer.launch()
-//   const page = await browser.newPage()
-//   await page.goto(url)
-//   const pdf = await page.pdf({ format: 'A4' })
-//   await browser.close()
-//   return pdf
-// }
+const createPDF = async ({ url = 'https://resume.josephrex.me' }) => {
+  const envOptions = process.env.ELEVENTY_ENV === 'production' ? {
+    executablePath: await chromium.executablePath,
+  } : {}
+  const browser = await chromium.puppeteer.launch({
+    args: chromium.args,
+    defaultViewport: chromium.defaultViewport,
+    headless: chromium.headless,
+    ...envOptions
+  })
+  const page = await browser.newPage()
+  await page.goto(url)
+  const pdf = await page.pdf({ format: 'A4' })
+  await browser.close()
+  return pdf
+}
 
-// exports.handler = async function(event) {
-//   const { queryStringParameters } = event
-//   const pdf = await createPDF(queryStringParameters)
-//   return {
-//     isBase64Encoded: true,
-//     statusCode: 200,
-//     headers: {
-//       'Content-Type': 'application/pdf',
-//       'Cache-control': `public, max-age=${30}`
-//     },
-//     body: Buffer.from(pdf).toString('base64')
-//   }
-// }
-exports.handler = async function() {
+exports.handler = async function(event) {
+  const { queryStringParameters } = event
+  const pdf = await createPDF(queryStringParameters)
   return {
-    body: 'Hello'
+    isBase64Encoded: true,
+    statusCode: 200,
+    headers: {
+      'Content-Type': 'application/pdf',
+      'Cache-control': `public, max-age=${30}`
+    },
+    body: Buffer.from(pdf).toString('base64')
   }
 }
