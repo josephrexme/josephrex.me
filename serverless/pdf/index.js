ecb6578ac08df75ba8544bae21dd45c2ea7cecbc
Joseph Rex <joerexko@gmail.com>
2021-12-30 03:13:48 -0600

    Comment puppeteer implementation out for functions byte limit

--- serverless/pdf/index.js
+++ serverless/pdf/index.js
@@ -1,24 +1,29 @@
-const puppeteer = require('puppeteer')
+// const puppeteer = require('puppeteer')
 
-const createPDF = async ({ url = 'https://resume.josephrex.me' }) => {
-  const browser = await puppeteer.launch()
-  const page = await browser.newPage()
-  await page.goto(url)
-  const pdf = await page.pdf({ format: 'A4' })
-  await browser.close()
-  return pdf
-}
+// const createPDF = async ({ url = 'https://resume.josephrex.me' }) => {
+//   const browser = await puppeteer.launch()
+//   const page = await browser.newPage()
+//   await page.goto(url)
+//   const pdf = await page.pdf({ format: 'A4' })
+//   await browser.close()
+//   return pdf
+// }
 
-exports.handler = async function(event) {
-  const { queryStringParameters } = event
-  const pdf = await createPDF(queryStringParameters)
+// exports.handler = async function(event) {
+//   const { queryStringParameters } = event
+//   const pdf = await createPDF(queryStringParameters)
+//   return {
+//     isBase64Encoded: true,
+//     statusCode: 200,
+//     headers: {
+//       'Content-Type': 'application/pdf',
+//       'Cache-control': `public, max-age=${30}`
+//     },
+//     body: Buffer.from(pdf).toString('base64')
+//   }
+// }
+exports.handler = async function() {
   return {
-    isBase64Encoded: true,
-    statusCode: 200,
-    headers: {
-      'Content-Type': 'application/pdf',
-      'Cache-control': `public, max-age=${30}`
-    },
-    body: Buffer.from(pdf).toString('base64')
+    body: 'Hello'
   }
 }
